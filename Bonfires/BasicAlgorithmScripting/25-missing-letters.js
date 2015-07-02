ac5c6f2491b7f92c04a81929959f0555fd373d79
Alexander Sidorov <shtil80@gmail.com>
2015-07-02 16:04:01 +0700

    Missing Letters Initial commit

---
+++ Bonfires/BasicAlgorithmScripting/25-missing-letters.js
@@ -0,0 +1,15 @@
+/*
+Find the missing letter in the passed letter range and return it.
+If all letters are present in the range, return undefined.
+
+Here are some helpful links:
+
+String.charCodeAt()
+String.fromCharCode()
+*/
+
+function fearNotLetter(str) {
+  return str;
+}
+
+fearNotLetter('abce');
