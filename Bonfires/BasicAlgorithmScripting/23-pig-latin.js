45fcc0702f2889bda9c3257c47bf5ba6e268aea8
Alexander Sidorov <shtil80@gmail.com>
2015-07-02 13:17:26 +0700

    Pig Latin done

---
+++ Bonfires/BasicAlgorithmScripting/23-pig-latin.js
@@ -0,0 +1,34 @@
+/*
+Translate the provided string to pig latin.
+Pig Latin takes the first consonant (or consonant cluster) of an English word, moves it to the end of the word and suffixes an "ay".
+If a word begins with a vowel you just add "way" to the end.
+
+Here are some helpful links:
+
+Array.indexOf()
+Array.push()
+Array.join()
+String.substr()
+String.split()
+*/
+
+function translate(str) {
+  var result = '';
+  var wowels = "aeiou";
+  var wowelsArr = [];
+  var firstWowel = 1;
+  if (/^[aeiou]/i.test(str) === true) {
+    return str + 'way';
+  } else {
+    for (var i in wowels) {
+      if (str.indexOf(wowels[i]) > 0) {
+        wowelsArr.push(str.indexOf(wowels[i]));
+      }
+    }
+    wowelsArr.sort();
+    console.log(wowelsArr);
+    firstWowel = wowelsArr[0];
+    return str.slice(firstWowel) + str.slice(0,firstWowel) + 'ay';
+  }
+}
+translate("brconsonant");
