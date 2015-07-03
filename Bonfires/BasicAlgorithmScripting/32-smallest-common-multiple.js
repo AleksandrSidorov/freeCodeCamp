22f00a263f5beb66e8ca0a77602399f4b607824d
sthil80 <sthil80>
2015-07-03 22:17:31 +0700

    Smallest Common Multiple Initial commit

---
+++ Bonfires/BasicAlgorithmScripting/32-smallest-common-multiple.js
@@ -0,0 +1,11 @@
+/*
+Find the smallest number that is evenly divisible by all numbers in the provided range.
+The range will be an array of two numbers that will not necessarily be in numerical order.
+*/
+
+function smallestCommons(arr) {
+  return arr;
+}
+
+
+smallestCommons([1,5]);
