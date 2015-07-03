8356a8ecfa2cfb282acc6dc58440ebe59166c34c
Alexander Sidorov <shtil80@gmail.com>
2015-07-03 14:51:42 +0700

    Convert HTML Entities Initial Commit

---
+++ Bonfires/BasicAlgorithmScripting/28-convert-html-entities.js
@@ -0,0 +1,15 @@
+/*
+Convert the characters "&", "<", ">", '"' (double quote), and "'" (apostrophe), in a string to their corresponding HTML entities.
+
+Here are some helpful links:
+
+RegExp
+HTML Entities
+*/
+
+function convert(str) {
+  // &colon;&rpar;
+  return str;
+}
+
+convert('Dolce & Gabbana');
