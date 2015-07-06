3be01025f0b359270ee9ddd62f95caef93f2836a
Alexander Sidorov <shtil80@gmail.com>
2015-07-06 15:56:50 +0700

    Binary Agents Complete

--- Bonfires/BasicAlgorithmScripting/36-binary-agents.js
+++ Bonfires/BasicAlgorithmScripting/36-binary-agents.js
@@ -9,7 +9,12 @@ String.fromCharCode()
 */
 
 function binaryAgent(str) {
-  return str;
+  var resultStr = '';
+  var myArr = str.split(' ');
+  var result = myArr.forEach(function(element) {
+    resultStr += String.fromCharCode(parseInt(element, 2));
+  });
+  return resultStr;
 }
 
 binaryAgent('01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111');
