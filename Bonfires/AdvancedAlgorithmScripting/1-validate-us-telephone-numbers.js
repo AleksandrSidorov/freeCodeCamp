577225124b0c3888186000d4116b55bf846849f7
Alexander Sidorov <shtil80@gmail.com>
2015-08-26 23:16:10 +0700

    Validate Us Phone Numbers Complete

--- Bonfires/AdvancedAlgorithmScripting/1-validate-us-telephone-numbers.js
+++ Bonfires/AdvancedAlgorithmScripting/1-validate-us-telephone-numbers.js
@@ -23,8 +23,8 @@ RegExp
 */
 
 function telephoneCheck(str) {
-  // Good luck!
-  return true;
+  var re = /^[1]?\s?[(]?\d{3}[)]?\s?[-]?\d{3}\s?[-]?\d{4}$/;
+  return re.test(str);
 }
 
 telephoneCheck("555-555-5555");
