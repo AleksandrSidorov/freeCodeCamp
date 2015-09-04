226f6efee18b27e90be3629ee09bb3cf9e117728
Aleksandr Sidorov <shtil80@gmail.com>
2015-09-04 21:49:41 +0700

    Finders Keepers V2 Added

--- Bonfires/IntermediateAlgorithmScripting/15-finders-keepers.js
+++ Bonfires/IntermediateAlgorithmScripting/15-finders-keepers.js
@@ -17,4 +17,9 @@ function find(arr, func) {
   return num;
 }
 
+function findV2(arr, func) {
+  var num = arr.filter(func);
+  return num[0];
+}
+
 find([1, 2, 3, 4], function(num){ return num % 2 === 0; });
