91ad3b4f34f217544ebfc3e4491bec7d2a98bcd3
Aleksandr Sidorov <shtil80@gmail.com>
2015-09-08 21:16:33 +0700

    Quote Machine Twitter Going on

--- Ziplines/BascFrontEndProjects/2-RandomQuoteMachine/js/main-jsonp.js
+++ Ziplines/BascFrontEndProjects/2-RandomQuoteMachine/js/main-jsonp.js
@@ -1,11 +1,37 @@
 function parseQuote(response) {
-  document.getElementById('quote').innerHTML = response.quoteText;
-  document.getElementById('author').innerHTML = response.quoteAuthor;
+  var qText = response.quoteText;
+  var qAuthor = response.quoteAuthor;
+  document.getElementById('quote').innerHTML = qText;
+  document.getElementById('author').innerHTML = qAuthor;
+  var tLink = document.getElementsByClass('twitter-share-button')[0];
+  var bLink = document.getElementById('b');
+  console.log(tLink);
+  console.log('href', 'https://twitter.com/intent/tweet?text=' + qText);
+  tLink.setAttribute('href', 'https://twitter.com/intent/tweet?text=' + qText);
+  bLink.setAttribute('href', 'https://twitter.com/intent/tweet?text=' + qText);
 }
 
 function getQuote() {
-  var apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=parseQuote';
-  var getScript = document.createElement('script');
-  getScript.src = apiUrl;
-  document.body.appendChild(getScript);
+  var scriptApi = document.createElement('script');
+  scriptApi.src = 'http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=parseQuote';
+  document.body.insertBefore(scriptApi, document.getElementsByName('title')[0]);
 }
+
+window.twttr = (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
+
+  t._e = [];
+  t.ready = function(f) {
+    t._e.push(f);
+  };
+
+  return t;
+}(document, "script", "twitter-wjs"));
+
+window.onload = getQuote();
