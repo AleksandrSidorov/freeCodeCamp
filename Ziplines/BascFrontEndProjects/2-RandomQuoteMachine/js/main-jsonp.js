f2d41651df8372d6a90689ce10e949cb910d2098
Alexander Sidorov <shtil80@gmail.com>
2015-09-09 17:36:22 +0700

    JSONP version complete. Need solve footer problem

--- Ziplines/BascFrontEndProjects/2-RandomQuoteMachine/js/main-jsonp.js
+++ Ziplines/BascFrontEndProjects/2-RandomQuoteMachine/js/main-jsonp.js
@@ -3,12 +3,23 @@ function parseQuote(response) {
   var qAuthor = response.quoteAuthor;
   document.getElementById('quote').innerHTML = qText;
   document.getElementById('author').innerHTML = qAuthor;
-  var tLink = document.getElementsByClass('twitter-share-button')[0];
-  var bLink = document.getElementById('b');
-  console.log(tLink);
-  console.log('href', 'https://twitter.com/intent/tweet?text=' + qText);
-  tLink.setAttribute('href', 'https://twitter.com/intent/tweet?text=' + qText);
-  bLink.setAttribute('href', 'https://twitter.com/intent/tweet?text=' + qText);
+  var tLink = document.getElementById('myTwi');
+  var tResultText = getTweetText(qText, qAuthor);
+  tLink.setAttribute('href', tResultText);
+}
+
+function getTweetText(tText, tAuthor) {
+  var tUrl = 'https://twitter.com/intent/tweet?text=';
+  if (tAuthor === '') {
+    tAuthor = 'Quote';
+  };
+  if ((tText + ' - ' + tAuthor).length <= 140) {
+    return tUrl + tText + ' - ' + tAuthor;
+  } else if (tText.length <=140) {
+    return tUrl + tText;
+  } else {
+    return tUrl + 'Quote too large for Twitter.';
+  }
 }
 
 function getQuote() {
@@ -16,22 +27,5 @@ function getQuote() {
   scriptApi.src = 'http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=parseQuote';
   document.body.insertBefore(scriptApi, document.getElementsByName('title')[0]);
 }
-
-window.twttr = (function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0],
-    t = window.twttr || {};
-  if (d.getElementById(id)) return t;
-  js = d.createElement(s);
-  js.id = id;
-  js.src = "https://platform.twitter.com/widgets.js";
-  fjs.parentNode.insertBefore(js, fjs);
-
-  t._e = [];
-  t.ready = function(f) {
-    t._e.push(f);
-  };
-
-  return t;
-}(document, "script", "twitter-wjs"));
 
 window.onload = getQuote();
