1807df13b65f9e2d2882aabdb4576e434d2af694
Alexander Sidorov <shtil80@gmail.com>
2015-09-12 20:28:48 +0700

    Background Change Mechanism

--- Ziplines/BascFrontEndProjects/3-current-weather/js/main.js
+++ Ziplines/BascFrontEndProjects/3-current-weather/js/main.js
@@ -1,11 +1,10 @@
-var ipLocatorUrl = 'http://ip-api.com/json';
 var owmApiKey = 'a5fa6deab88fe51ae1f33c01dfe97ab2';
 var owmUrl = 'http://api.openweathermap.org/data/2.5/weather?callback=?';
 var fCountries = ['BS', 'BZ', 'KY', 'PW', 'US', 'PR', 'AS'];
 
 $(document).ready(function() {
   $.ajax({
-    url: ipLocatorUrl,
+    url: "http://ip-api.com/json",
     success: function(response) {
       var useUnits = 'metric';
       if (fCountries.indexOf(response.countryCode) > 0) {
@@ -34,11 +33,56 @@ function parseWeather(data) {
   if (data.dt >= data.sys.sunrise && data.dt < data.sys.sunset) {
     var dayTime = '-day-';
   }
-  var owmIconClass = 'wi wi-owm' + dayTime + data.weather[0].id;
-  var owmWindIconClass = 'wi wi-wind from-' + data.wind.deg + '-deg';
-  $( "#owm-temp" ).html(data.main.temp);
-  $( "#owm-icon" ).removeClass().addClass(owmIconClass);
+  var useUnitLetter = ' &#176;C';
+  if (fCountries.indexOf(data.sys.country) > 0) {
+    useUnitLetter = ' &#176;F';
+  }
+  var owmIconStr = 'wi wi-owm' + dayTime + data.weather[0].id;
+  var windClass = 'wi wi-wind from-' + data.wind.deg + '-deg';
+  console.log(data.weather[0].main);
+  
+  $( "body" ).css("background", "url(" + getBackground(data.weather[0].main) + ") no-repeat center center fixed");
+  $( "#owm-temp" ).html(data.main.temp + useUnitLetter);
+  $( "#owm-temp-max span" ).html(data.main.temp_max + useUnitLetter);
+  $( "#owm-temp-min span" ).html(data.main.temp_min + useUnitLetter);
+  $( "#owm-humidity span" ).html(data.main.humidity + '%');
+  $( "#owm-temp" ).html(data.main.temp + useUnitLetter);
+  $( "#owm-icon" ).removeClass().addClass(owmIconStr);
   $( "#owm-description" ).html(data.weather[0].description);
   $( "#owm-wind-speed" ).html(data.wind.speed + ' m/s');
-  $( "#owm-wind-icon" ).removeClass().addClass(owmWindIconClass);
+  $( "#owm-wind-icon" ).removeClass().addClass(windClass);
   }
+
+function getBackground(mainCondition) {
+  var bgUrl;
+  switch(mainCondition) {
+    case "Thunderstorm":
+      bgUrl = "https://drive.google.com/uc?export=download&id=0B41DdgPqKzCaaW1vVUVjRTBuX3c";
+      break;
+    case "Drizzle":
+      bgUrl = "https://drive.google.com/uc?export=download&id=0B41DdgPqKzCaempENF8ycUFHX2M";
+      break;
+    case "Rain":
+      bgUrl = "https://drive.google.com/uc?export=download&id=0B41DdgPqKzCaempENF8ycUFHX2M";
+      break;
+    case "Snow":
+      bgUrl = "https://drive.google.com/uc?export=download&id=0B41DdgPqKzCaWkhXU2dra1RhbTQ";
+      break;
+    case "Atmosphere":
+      bgUrl = "https://drive.google.com/uc?export=download&id=0B41DdgPqKzCaOWFHSGY5MUt5Qm8";
+      break;
+    case "Clouds":
+      bgUrl = "https://drive.google.com/uc?export=download&id=0B41DdgPqKzCacHBEamd3Q2ZpYTA";
+      break;
+    case "Extreme":
+      bgUrl = "https://drive.google.com/uc?export=download&id=0B41DdgPqKzCaaW1vVUVjRTBuX3c";
+      break;
+    case "Additional":
+      bgUrl = "https://drive.google.com/uc?export=download&id=0B41DdgPqKzCacHBEamd3Q2ZpYTA";
+      break;
+    default:
+      bgUrl = "https://drive.google.com/uc?export=download&id=0B41DdgPqKzCacHBEamd3Q2ZpYTA";
+  }
+  console.log(bgUrl);
+  return bgUrl;
+}
