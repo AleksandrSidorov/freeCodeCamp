376f40867fc48f27e0bcb1854515c3f608f6603d
Aleksandr Sidorov <shtil80@gmail.com>
2015-09-13 20:15:31 +0700

    Max and Min Temperature come from daily forecast data.

--- Ziplines/BascFrontEndProjects/3-current-weather/js/main.js
+++ Ziplines/BascFrontEndProjects/3-current-weather/js/main.js
@@ -1,5 +1,6 @@
 var owmApiKey = 'a5fa6deab88fe51ae1f33c01dfe97ab2';
 var owmUrl = 'http://api.openweathermap.org/data/2.5/weather?callback=?';
+var owmDailyUrl = 'http://api.openweathermap.org/data/2.5/forecast/daily?callback=?';
 var fCountries = ['BS', 'BZ', 'KY', 'PW', 'US', 'PR', 'AS'];
 
 $(document).ready(function() {
@@ -16,8 +17,15 @@ $(document).ready(function() {
            {q: ipCity,
             type: 'like',
             units: useUnits,
-            APPID: owmUrl})
+            APPID: owmApiKey})
        .done(parseWeather);
+       $.getJSON(owmDailyUrl,
+            {q: ipCity,
+             type: 'like',
+             cnt: '1',
+             units: useUnits,
+             APPID: owmApiKey})
+        .done(parseDailyWeather);
     },
     dataType: 'json',
     statusCode: {
@@ -33,25 +41,32 @@ function parseWeather(data) {
   if (data.dt >= data.sys.sunrise && data.dt < data.sys.sunset) {
     var dayTime = '-day-';
   }
-  var useUnitLetter = ' &#176;C';
+  var useUnitLetter = '&#176;C';
   if (fCountries.indexOf(data.sys.country) > 0) {
-    useUnitLetter = ' &#176;F';
+    useUnitLetter = '&#176;F';
   }
   var owmIconStr = 'wi wi-owm' + dayTime + data.weather[0].id;
   var windClass = 'wi wi-wind from-' + data.wind.deg + '-deg';
   console.log(data.weather[0].main);
-  
+
   $( "body" ).css("background", "url(" + getBackground(data.weather[0].main) + ") no-repeat center center fixed");
   $( "#owm-temp" ).html(data.main.temp + useUnitLetter);
-  $( "#owm-temp-max span" ).html(data.main.temp_max + useUnitLetter);
-  $( "#owm-temp-min span" ).html(data.main.temp_min + useUnitLetter);
   $( "#owm-humidity span" ).html(data.main.humidity + '%');
   $( "#owm-temp" ).html(data.main.temp + useUnitLetter);
   $( "#owm-icon" ).removeClass().addClass(owmIconStr);
   $( "#owm-description" ).html(data.weather[0].description);
   $( "#owm-wind-speed" ).html(data.wind.speed + ' m/s');
   $( "#owm-wind-icon" ).removeClass().addClass(windClass);
+}
+
+function parseDailyWeather(data) {
+  var useUnitLetter = '&#176;C';
+  if (fCountries.indexOf(data.city.country) > 0) {
+    useUnitLetter = '&#176;F';
   }
+  $( "#owm-temp-max span" ).html(data.list[0].temp.max + useUnitLetter);
+  $( "#owm-temp-min span" ).html(data.list[0].temp.min + useUnitLetter);
+}
 
 function getBackground(mainCondition) {
   var bgUrl;
