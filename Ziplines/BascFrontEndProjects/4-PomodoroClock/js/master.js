cc2203f0b34190e22a26c861f1b6eb9c6c2a737f
Alexander Sidorov <shtil80@gmail.com>
2015-09-16 17:12:45 +0700

    Buttons Works

--- Ziplines/BascFrontEndProjects/4-PomodoroClock/js/master.js
+++ Ziplines/BascFrontEndProjects/4-PomodoroClock/js/master.js
@@ -1,8 +1,8 @@
 var clockIsRunning = false;
 var sessionNow = true;
 var sessionNames = ['Break', 'Session'];
-var breakLength = 1;
-var sessionLength = 1;
+var breakLength = 5;
+var sessionLength = 25;
 var pomoIntervalId;
 var currentSeconds, hours, minutes, seconds;
 
@@ -37,6 +37,26 @@ $(document).ready(function() {
   $(".session-value").html(sessionLength);
   $(".period-name").html(sessionNames[Number(sessionNow)]);
   $(".timer-value").html(sessionLength + ":00");
+
+  $("#break-minus").click(function() {
+    console.log("cococo");
+    breakLength = breakLength <= 0 ? 0 : breakLength - 1;
+    console.log(breakLength);
+    $(".break-value").html(breakLength);
+  });
+  $("#break-plus").click(function() {
+    breakLength = breakLength >= 1440 ? 1440 : breakLength + 1;
+    $(".break-value").html(breakLength);
+  });
+  $("#session-minus").click(function() {
+    sessionLength = sessionLength <= 0 ? 0 : sessionLength - 1;
+    $(".session-value").html(sessionLength);
+  });
+  $("#session-plus").click(function() {
+    sessionLength = sessionLength >= 1440 ? 1440 : sessionLength + 1;
+    $(".session-value").html(sessionLength);
+  });
+
   $(".clock").click(function() {
     if (clockIsRunning) {
       stopTimer();
