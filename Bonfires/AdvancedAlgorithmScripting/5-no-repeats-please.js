4e6f2eeb7dc9be6fe3099f7efae6189afba99fc2
Alexander Sidorov <shtil80@gmail.com>
2015-09-29 17:26:23 +0700

    No Repeats Please Try to permutate

--- Bonfires/AdvancedAlgorithmScripting/5-no-repeats-please.js
+++ Bonfires/AdvancedAlgorithmScripting/5-no-repeats-please.js
@@ -11,7 +11,24 @@ RegExp
 */
 
 function permAlone(str) {
-  return str;
+  var results = [];
+
+  function perm(input, memo) {
+    var cur, memo = memo || "";
+
+    for (var i = 0; i < input.length; i++) {
+      cur = input.substring(i);
+      if (input.length === 0) {
+        results.push(memo + cur);
+      }
+      perm(input.slice(0), memo + cur);
+      input = input.slice(0, i) + (cur[0] || "") + input.slice(i);
+    }
+
+    return results;
+  }
+
+  return perm(str);
 }
 
 permAlone('aab');
