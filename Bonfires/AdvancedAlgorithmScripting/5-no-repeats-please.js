6cb956167b17ba4858c3360b2b12c9f54c293429
Aleksandr Sidorov <shtil80@gmail.com>
2015-09-30 21:40:07 +0700

    No Repeats Please v2

--- Bonfires/AdvancedAlgorithmScripting/5-no-repeats-please.js
+++ Bonfires/AdvancedAlgorithmScripting/5-no-repeats-please.js
@@ -10,25 +10,33 @@ Permutations
 RegExp
 */
 
-function permAlone(str) {
-  var results = [];
-
-  function perm(input, memo) {
-    var cur, memo = memo || "";
-
-    for (var i = 0; i < input.length; i++) {
-      cur = input.substring(i);
-      if (input.length === 0) {
-        results.push(memo + cur);
-      }
-      perm(input.slice(0), memo + cur);
-      input = input.slice(0, i) + (cur[0] || "") + input.slice(i);
+function permAlone(str){
+    //Enclosed data to be used by the internal recursive function permutate():
+    var permutations = [],  //generated permutations stored here
+        nextWord = [],      //next word builds up in here
+        chars = []          //collection for each recursion level
+    ;
+    //---------------------
+    //split words or numbers into an array of characters
+    if (typeof str === 'string') chars = str.split('');
+    else if (typeof str === 'number') {
+      str = str + ""; //convert number to string
+      chars = str.split('');//convert string into char array
     }
-
-    return results;
-  }
-
-  return perm(str);
-}
-
+    //============TWO Declaratives========
+    permutate(chars);
+    console.log(permutations, permutations.length); 
+    return permutations;
+    //===========UNDER THE HOOD===========
+    function permutate(chars){ //recursive: generates the permutations
+        if(chars.length === 0)permutations.push(nextWord.join(''));
+        for (var i=0; i < chars.length; i++){
+            chars.push(chars.shift());  //rotate the characters
+            nextWord.push(chars[0]);    //use the first char in the array
+            permutate(chars.slice(1));  //Recurse: array-less-one-char
+            nextWord.pop();             //clear for nextWord (multiple pops)
+        }
+    }
+    //--------------------------------
+}//==============END of getPermutations(str)=============
 permAlone('aab');
